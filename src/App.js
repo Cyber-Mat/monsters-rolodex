ce36cffccb2063d2093e185de893ce5fc248e2e1
cyber-mat <olaniyanolamide@gmail.com>
2020-07-24 11:42:45 +0100

    Card component complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
 //import { Component } from 'react';
-
+import axios from 'axios';
+import { CardList } from './components/card-list/card-list.component';
 class App extends React.Component {
   constructor() {
     super();
@@ -22,9 +21,7 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
   }
