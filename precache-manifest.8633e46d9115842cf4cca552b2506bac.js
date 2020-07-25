40ac5e66f0986205977ef8892a44ba7e2ce0c9c5
cyber-mat <olaniyanolamide@gmail.com>
2020-07-25 05:27:26 +0100

    Updates

---
+++ precache-manifest.8633e46d9115842cf4cca552b2506bac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cced43335c94a3c0fa359a6fc44f801",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "55a7544fd3ac2cc768aa",
+    "url": "/monsters-rolodex/static/css/main.915bbbc5.chunk.css"
+  },
+  {
+    "revision": "addab9ad8c8c40c96cb7",
+    "url": "/monsters-rolodex/static/js/2.58cce731.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.58cce731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a7544fd3ac2cc768aa",
+    "url": "/monsters-rolodex/static/js/main.cea007b7.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
